d0ea543c43e8fb078a81a9a4e1015a7aac706991
joviscab <152897880+joviscab@users.noreply.github.com>
2024-09-16 01:59:27 +0200

    add midmenu, rename some scenes and organize project structure

--- scripts/finish.gd
+++ scripts/finish.gd
@@ -3,4 +3,4 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if (body.name == "CharacterBody2D"):
-		get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+		get_tree().change_scene_to_file("res://scenes/mid_menu.tscn")
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -5,5 +5,9 @@ extends Node
 func _on_new_game_pressed() -> void:
 	dog_bark.play()
 	await get_tree().create_timer(0.5).timeout
-	get_tree().change_scene_to_file("res://main.tscn")
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
 	
+
+
+func _on_main_menu_pressed() -> void:
+	pass # Replace with function body.
