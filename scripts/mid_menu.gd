98fe5816b275ad297ec6cd2626a4992566226cc1
joviscab <152897880+joviscab@users.noreply.github.com>
2024-09-16 03:08:24 +0200

    add main menu logo animation, sorriso animation and background animation

--- scripts/mid_menu.gd
+++ scripts/mid_menu.gd
@@ -3,10 +3,6 @@ extends Node
 @onready var dog_bark: AudioStreamPlayer2D = $dog_bark
 
 
-
-
-
-
 func _on_main_menu_pressed() -> void:
 	dog_bark.play()
 	await get_tree().create_timer(0.5).timeout
